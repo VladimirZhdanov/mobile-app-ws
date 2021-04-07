98360d608c08fe487c1de3142b68e7b611e31a9d
VladimirZhdanov <ConstHomespb@gmail.com>
2021-04-07 20:55:14 +0300

    Revert "1st commit"
    This reverts commit 5e159c24

--- src/main/java/com/homel/project/app/ws/io/entity/UserEntity.java
+++ src/main/java/com/homel/project/app/ws/io/entity/UserEntity.java
@@ -10,9 +10,6 @@ public class UserEntity implements Serializable {
 
     private static final long serialVersionUID = -7949261955411780183L;
 
-    private static final String COMMIT_1 = "blabla";
-    private static final String COMMIT_2 = "blabla2";
-
     @Id
     @GeneratedValue
     private long id;
